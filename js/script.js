0a1791adc7371a6c3465d0c9f5aaa26c99580879
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-16 15:12:58 +0100

    esercizio 1 *fine* + inizio esercizio 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata
+*/
+
+/*creo input e lo converto in lowercase*/
+let inputParola = prompt("Inserisci qui la tua parola!");
+
+let parolaLowerCase = inputParola.toLowerCase();
+/*inserisco la parola in un array*/
+let arrayParola = parolaLowerCase.split("");
+/*definisco la funzione per capire se la stringa e' paliondroma*/
+function palindrome(array){
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== array[array.length - 1 - i]) {
+            return false;
+        }   
+    }
+ return true;
+}
+
+/*richiamo la funzione per analizzare l'array*/
+let isPalindromo = palindrome(arrayParola);
+
+/*printo in console la soluzione*/
+if (isPalindromo == true){
+    console.log("La stringa "+inputParola+" e' palindroma ");
+}else{
+    console.log("La stringa "+inputParola+" non e' palindroma ");
+}
+
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    ESERCIZIO 2    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+
+
+
+
 
